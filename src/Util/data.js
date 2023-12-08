941720fa3a10d8a4e246d8ca853b0b72ed220387
janitchawla <janitchawla31@gmail.com>
2023-12-08 19:29:04 +0530

    links update

--- src/Util/data.js
+++ src/Util/data.js
@@ -116,38 +116,38 @@ export const WORK = [
         title: "Node.js CI/CD with Load Balancer",
         description: "Created jenkins pipeline that does the following: Checkout nodejs code from github -> Build the code and pushes the artifact to nexus -> fetches the artifact and pushes it to VM’s through ansible and unzip the artifact -> Start the nodejs server on 2 Vm’s using ansible. Pipeline gives the option to update the version of code from v1 to v2. Tried to achieve near 0 downtime while updating the application: used rolling update Implemented a loadbalancer and health checks",
         description2: "Jenkins, Nexus, Node, Systemd, Loadbalancer, Ansible ",
-        ghlink: "https://github.com/ywalia01/dune-api-frontend",
-        extlink: "https://ywalia01.github.io/dune-api-frontend/",
+        ghlink: "https://github.com/JanitChawla/Node-ansible",
+        // extlink: "https://ywalia01.github.io/dune-api-frontend/",
     },
     {
         title: "CI/CD for kubernetes deployment",
         description:
             "Running Jenkins in a kubernetes cluster. Created a jenkins pipeline for building docker image and pushing to docker hub -> Pulling the image and deploying it on a kubernetes cluster using yaml files. Also implemented webhooks in it so as whenever there is a push to github repo, pipeline should automatically get triggered. This helped me learn about kubernetes authentication with service accounts, config file and tokens",
         description2: "Jenkins, Kubernetes, Webhooks",
-        ghlink: "https://github.com/ywalia01/dune-api",
-        extlink: "https://the-dune-api.herokuapp.com/quotes/1",
+        ghlink: "https://github.com/JanitChawla/Jenkins",
+        // extlink: "https://the-dune-api.herokuapp.com/quotes/1",
     },
     {
         title: "Containerized Application",
         description:
             "Containerized notejam application using docker and kubernetes. Created Docker file for the django project and connected it with postgres container Docker image: https://hub.docker.com/r/janit31/notejam-webapp",
         description2: "Docker",
-        ghlink: "",
-        extlink: "https://www.npmjs.com/package/random-dune-quotes",
+        ghlink: "https://github.com/JanitChawla/Notejam-Containerzed",
+        // extlink: "https://www.npmjs.com/package/random-dune-quotes",
     },
     {
         title: "Kubernetes Deployment",
         description: "Created a docker images for a fullstack application. Using kubernetes deployed application, created deployments, service, ingress, persistent volume, pvc, secrets, config-map",
         description2: "Docker, Kubernetes",
-        ghlink: "https://github.com/ywalia01/ywalia01.github.io",
-        extlink: "https://ywalia01.netlify.app/",
+        ghlink: "https://github.com/JanitChawla/Notejam-Containerzed/tree/main/notejam/k8",
+        // extlink: "https://ywalia01.netlify.app/",
     },
     {
         title: "Umbrella Helm",
         description: "Created Umbrella Helm chart for the app deployment",
         description2: "Helm, Kubernetes",
-        ghlink: "https://github.com/ywalia01/artist-portfolio",
-        extlink: "https://artist-pfolio.netlify.app/",
+        ghlink: "https://github.com/JanitChawla/Helm",
+        // extlink: "https://artist-pfolio.netlify.app/",
     },
 ];
 
